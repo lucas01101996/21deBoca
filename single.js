f8307107fa178a7d778ccb1855170937f2e9c123
FELIPE MAGALHAES SALES <202208863621@alunos.uniruy.edu.br>
2023-09-06 21:29:23 -0300

    testando funcionalidade js

--- single.js
+++ single.js
@@ -8,28 +8,40 @@ var pontuacao = document.getElementById("pontuacao");
 var textLance = document.createTextNode("0"); // aumentar o numero e criar funçoes para quando clicar provavelem usa if e for
 
  
+// var soma = 0;
+// // tenho que fazer um botao que quando clicar irá mudar seu valor para 1,2 ou 3 a mais do que o valor atual
 
-// tenho que fazer um botao que quando clicar irá mudar seu valor para 1,2 ou 3 a mais do que o valor atual
-
- pontuacao.Value = 0;   
+ pontuacao.Value = 0;
  textLance = 0;
 
-
-btnLance1.onclick = function(){ 
-    pontuacao.innerHTML =(pontuacao.Value)+1;
-  
-  
+ 
+ function somaFuncao(pontuacao, soma) {
+    while (pontuacao<=21) {
+      pontuacao = pontuacao+soma
+    }
+    return pontuacao;
 }
-btnLance2.onclick = function(){
-    pontuacao.innerHTML = (pontuacao.Value)+2;
-  }
 
-btnLance3.onclick = function(){
-    pontuacao.innerHTML = (pontuacao.Value)+3;
-  }
+      
+    btnLance1.onclick = function (){ 
+     pontuacao.innerHTML = somaFuncao(pontuacao.Value,1);
+     console.log(pontuacao.innerHTML)  
+     console.log(pontuacao) 
+    }
 
-  console.log(pontuacao.Value)
-pontuacao.appendChild(textLance);
+    btnLance2.onclick = function (){
+        pontuacao.innerHTML = somaFuncao(pontuacao.Value,2);
+        console.log(pontuacao.Value) 
+      }
+
+    btnLance3.onclick = function (){
+        pontuacao.innerHTML = somaFuncao(pontuacao.Value,3);
+        console.log(pontuacao.Value)
+      }
+ 
+  
+console.log(pontuacao.Value)
+//pontuacao.appendChild(textLance);
 
 
 // fazer de forma sorteada a vez do computador o professor deu a função
